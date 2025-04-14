957333090e383d9cebff16e8e7267c8e113d556d
Gabriel Delaneza <gabriel.delaneza@enacom.com.br>
2025-04-14 11:28:48 -0300

    removed console.log

--- src/api/author/useCases/indexAuthorUseCase.js
+++ src/api/author/useCases/indexAuthorUseCase.js
@@ -1,7 +1,6 @@
 import Author from '../model/model.js'
 
 export const indexAuthorsUseCase = async ({ query }) => {
-  console.log(query)
   const author = await Author.findAll({ where: query })
   return author
 }
