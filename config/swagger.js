fb42381aab796caf58b9e3b502a1236003602c57
Gabriel Delaneza <gabriel.delaneza@enacom.com.br>
2025-04-14 11:09:20 -0300

    added swagger config file

---
+++ config/swagger.js
@@ -0,0 +1,16 @@
+import swaggerJSDoc from 'swagger-jsdoc'
+
+const options = {
+  definition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'API da API',
+      version: '1.0.0',
+    },
+  },
+  apis: ['./src/api/**/*.js'], // Caminho dos arquivos com os comentários JSDoc
+}
+
+const swaggerSpec = swaggerJSDoc(options)
+
+export default swaggerSpec
